9417b0127fc00ff7c91dc92ba19806381bfd889c
Anton Eriksson <145985545+aner2308@users.noreply.github.com>
2023-10-12 07:09:42 +0200

    Lade till filer från mall-repositorie
    Flyttade projektet till mitt eget repository för att kunna publicera sidan

---
+++ README.md
@@ -0,0 +1,9 @@
+# Labbgrund till Moment 4 i kursen DT084G, Introduktion till programmering i JavaScript
+*Anton Eriksson, aner2308*
+
+Detta repo är skapad av mallen till moment 4.
+Laborationen går ut på att skapa en funktion i **JavaScript** som används för att lagra en *"att göra"*-lista på webbsidan. 
+
+Användaren skall kunna fylla på nya *list-items* i listan, och klarmarkera *list-items* som man vill ta bort. Det ska också finnas en fungerande rensa-knapp som raderar hela listan.
+
+Om sidan laddas om ska listan laddas in i sitt senaste format, med hjälp av local storage.
---
+++ js/main.js
@@ -0,0 +1,137 @@
+/*
+Här lägger du din JavaScript-kod
+*/
+
+//Variabler
+
+let addNewToDoEl = document.getElementById("newtodo");
+let addToDoButtonEl = document.getElementById("newtodobutton");
+let messageEl = document.getElementById("message");
+let toDoListEl = document.getElementById("todolist");
+let clearButtonEl = document.getElementById("clearbutton");
+let i;
+
+//Händelsehanterare
+
+addNewToDoEl.addEventListener("keyup", checkInput, false);
+addToDoButtonEl.addEventListener("click", newToDo, false);
+clearButtonEl.addEventListener("click", clearAll, false);
+window.onload = init;
+
+//Start-funktion
+function init() {
+    console.log("initierar...");
+
+    //Avaktiverar lägg till-knappen
+    addToDoButtonEl.disabled = true;
+
+    //läs in lista
+    loadList();
+}
+
+//kontrollera input
+function checkInput() {
+    console.log("Kontrollerar input...");
+
+    let input = addNewToDoEl.value;
+
+    //kontroll av längd
+    if (input.length > 4) {
+        addToDoButtonEl.disabled = false;
+        messageEl.innerHTML = "";
+    } else {
+        messageEl.innerHTML = "Måste innehålla minst 5 tecken";
+        addToDoButtonEl.disabled = true;
+    }
+}
+
+//Lägg till kurs
+function newToDo() {
+    console.log("Lägger till att göra...")
+
+    //Skapar ny "att göra"- text
+    let input = addNewToDoEl.value;
+    let newEl = document.createElement("article");
+    let newTextNodeEl = document.createTextNode(input);
+    newEl.appendChild(newTextNodeEl);
+    newEl.className = "todo";
+
+    newEl.addEventListener("click", function (e) {
+        e.target.remove();
+    });
+
+    //Lägger till texten i att göra listan
+    toDoListEl.appendChild(newEl);
+
+    //Tar bort vår text i input- rutan
+    addNewToDoEl.value = "";
+    addToDoButtonEl.disabled = true;
+
+    //anropar lagring och sparar til local storage
+    saveToDo();
+}
+
+//rensa listan med rensa-knapp
+function clearAll() {
+    let element = document.getElementsByClassName("todo");
+    while (element.length > 0) {
+      element[0].parentNode.removeChild(element[0]);
+    }
+    
+    saveToDo();
+  }
+
+//Sparar listan
+function saveToDo() {
+    console.log("Lagrar listan...");
+
+    //Läser in "att göra"-listan
+    let toDoList = document.getElementsByClassName("todo");
+
+    //skapar en temporär array för vår lista
+    let tempArr = [];
+
+    //Loopar igenom listan och lagrar innehålet till en array
+    for (i = 0; i < toDoList.length; i++) {
+        tempArr.push(toDoList[i].innerHTML);
+    }
+
+    //lagrar vår array i en JSON string
+    let jsonStr = JSON.stringify(tempArr);
+
+    //Lagrar vår string på local storage
+    localStorage.setItem("list", jsonStr)
+
+    //Kan tas bort
+    console.log(tempArr);
+}
+
+//Läs in kurser
+function loadList() {
+    console.log("Läser in listan...")
+
+    //Hämtar vår json string och konverterar den tillbaka till en array
+    let toDoList = JSON.parse(localStorage.getItem("list"));
+
+    //Loop som går igenom vår sparade array
+    for (i = 0; i < toDoList.length; i++) {
+
+        //Återskapar "att göra"- text
+        let newEl = document.createElement("article");
+        let newTextNodeEl = document.createTextNode(toDoList[i]);
+        newEl.appendChild(newTextNodeEl);
+        newEl.className = "todo";
+
+        //Lägger till texten i att göra listan
+        toDoListEl.appendChild(newEl);
+
+        //tar bort item vid klick
+        newEl.addEventListener("click", function (e) {
+            e.target.remove();
+
+            saveToDo();
+        });
+
+    }
+    console.log(toDoList);
+}
